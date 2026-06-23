46ce5b66558a94538a74805e1672582eacef14e3
zhangdehao087 <zhangdehao087@qq.com>
2026-06-23 21:46:04 +0800

    ex03-1：新建SOS工程，定义课件标准时序常量与LED引脚

---
+++ ex03/ex03.ino
@@ -0,0 +1,16 @@
+// ex03 millis SOS求救信号灯 ESP32 GPIO2
+const int ledPin = 2;
+// SOS时序参数
+const int SHORT_ON = 200;
+const int LONG_ON = 600;
+const int DOT_GAP = 200;
+const int CHAR_GAP = 500;
+const int SOS_GAP = 2000;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
