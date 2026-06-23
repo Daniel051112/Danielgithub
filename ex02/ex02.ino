21b12f383f22335d04cb852eae41e4f390da59a9
zhangdehao087 <zhangdehao087@qq.com>
2026-06-23 21:38:34 +0800

    ex02-3：对齐课件代码注释格式，增加串口打印，完成最终1Hz闪烁程序

--- ex02/ex02.ino
+++ ex02/ex02.ino
@@ -1,10 +1,15 @@
-// ex02 millis 1Hz闪烁 ESP32 GPIO2
+// 定义LED引脚，ESP32通常板载LED连接在GPIO 2
 const int ledPin = 2;
+// millis计时相关变量
 unsigned long lastTime = 0;
+// 500ms切换一次亮灭，周期1000ms = 1Hz
 const unsigned long interval = 500;
 bool ledState = LOW;
 
 void setup() {
+  // 初始化串口通信，设置波特率为115200
+  Serial.begin(115200);
+  // 将LED引脚设置为输出模式
   pinMode(ledPin, OUTPUT);
 }
 
@@ -14,5 +19,10 @@ void loop() {
     lastTime = currentTime;
     ledState = !ledState;
     digitalWrite(ledPin, ledState);
+    if(ledState){
+      Serial.println("LED ON");  // 串口输出提示
+    }else{
+      Serial.println("LED OFF"); // 串口输出提示
+    }
   }
 }
