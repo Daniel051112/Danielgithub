a56375ed57a69d575c84cf59c8ab0731c24199b4
zhangdehao087 <zhangdehao087@qq.com>
2026-06-23 21:34:18 +0800

    ex02-1：新建工程，定义LED引脚与millis计时变量框架

---
+++ ex02/ex02.ino
@@ -0,0 +1,13 @@
+// ex02 millis 1Hz闪烁 ESP32 GPIO2
+const int ledPin = 2;
+unsigned long lastTime = 0;
+const unsigned long interval = 500;
+bool ledState = LOW;
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
